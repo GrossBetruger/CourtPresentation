7ee97969bbbe8ddb8799111c5440584c37c4333a
Oren Koriat <oren@cynerio.co>
2019-07-06 15:01:08 +0300

    exclude infinity, normalize to mbps

--- download_rate_different_sources_comparison.sql
+++ download_rate_different_sources_comparison.sql
@@ -1,3 +1,6 @@
-select file_name, avg(file_download_ratekbper_sec) from file_download_info 
+select file_name, avg(file_download_ratekbper_sec / 1024 * 8) 
+from file_download_info 
+where file_download_ratekbper_sec != 'infinity'
 group by file_name
 order by avg
+
