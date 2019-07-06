c84a218327e960ea8bf00ee4faa7037c08ee2e65
Oren Koriat <oren@cynerio.co>
2019-07-06 15:37:03 +0300

    remove website table from ground truth queries

--- oversell_40mbps.sql
+++ oversell_40mbps.sql
@@ -10,14 +10,12 @@ rush_hour_speed_tests as (
   select *, user_details.user_name as name
   from comparison_info
   join file_download_info on comparison_info.file_download_info_id = file_download_info.id
-  join speed_test_web_site on speed_test_web_site.id = comparison_info.speed_test_web_site_download_info_id
   join test t on comparison_info.test_id = t.id
   join user_details on t.user_name = user_details.user_name and user_details.speed != 1000
   where file_download_ratekbper_sec != 'infinity'
   and speed = (select * from target_program)
-  and date_part('hour', to_timestamp(start_measuring_timestamp/ 1000) at time zone 'IST' + '1 hour')::int  between 18 and 23 -- evening
-  and date_part('day', to_timestamp(start_measuring_timestamp/ 1000) at time zone 'IST' + '1 hour') in (3, 5, 6, 7) -- "Thursday, Friday, Saturday, Tuesday"
-  and (speed_test_web_site.analyzed_state = 0 or t.is_classic_test = false)
+  and date_part('hour', to_timestamp(start_downloading_timestamp/ 1000) at time zone 'IST' + '1 hour')::int  between 18 and 23 -- evening
+  and date_part('day', to_timestamp(start_downloading_timestamp/ 1000) at time zone 'IST' + '1 hour') in (3, 5, 6, 7) -- "Thursday, Friday, Saturday, Tuesday"
   ),
 
 user_rush_avg as (
