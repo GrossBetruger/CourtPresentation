b662c3485ef039e023d5a6ccf36401db852c2f21
Oren Koriat <oren@cynerio.co>
2020-07-03 20:35:57 +0300

    capacity of service queries added

---
+++ queries/capacity_of_service_40_lan_rush_hours_israel_cache.sql
@@ -0,0 +1,11 @@
+select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
+       count(*)
+from valid_tests
+where speed = 40
+and connection = 'LAN'
+and file_name = 'israel_cache'
+and rush_hour(timestamp)
+group by capacity(ground_truth_rate, speed)
+order by capacity(ground_truth_rate, speed)
+;
+
