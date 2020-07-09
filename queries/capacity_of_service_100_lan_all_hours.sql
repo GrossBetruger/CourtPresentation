58726e653ec63a1bfd693ef3b26ba6efcb44a049
Oren Koriat <oren@cynerio.co>
2020-07-09 15:37:57 +0300

    delete old queries

--- queries/capacity_of_service_100_lan_all_hours.sql
+++
@@ -1,9 +0,0 @@
-select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
-       count(*)
-from valid_tests
-where speed = 100
-and connection = 'LAN'
-group by capacity(ground_truth_rate, speed)
-order by capacity(ground_truth_rate, speed)
-;
-
--- queries/capacity_of_service_100_lan_rush_hours.sql
+++
@@ -1,9 +0,0 @@
-select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', ''),
-       count(*)
-from valid_tests
-where speed = 100
-and connection = 'LAN'
-and rush_hour(timestamp)
-group by capacity(ground_truth_rate, speed)
-order by capacity(ground_truth_rate, speed)
-;
