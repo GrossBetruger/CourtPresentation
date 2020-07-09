af019edf24754633cdee2d2d20baa8bc210444e8
Oren Koriat <oren@cynerio.co>
2020-07-09 15:36:28 +0300

    capacity of service moved to own dir

---
+++ queries/capacity_of_service/capacity_of_service_100_lan_all_hours.sql
@@ -0,0 +1,9 @@
+select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
+       count(*)
+from valid_tests
+where speed = 100
+and connection = 'LAN'
+group by capacity(ground_truth_rate, speed)
+order by capacity(ground_truth_rate, speed)
+;
+
---
+++ queries/capacity_of_service/capacity_of_service_40_lan_rush_hours.sql
@@ -0,0 +1,10 @@
+select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
+       count(*)
+from valid_tests
+where speed = 40
+and connection = 'LAN'
+and rush_hour(timestamp)
+group by capacity(ground_truth_rate, speed)
+order by capacity(ground_truth_rate, speed)
+;
+
