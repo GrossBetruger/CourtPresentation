2f4d53ae27af845cf6fe43855c088584e37d63fa
Oren Koriat <oren@cynerio.co>
2020-09-20 17:53:17 +0300

    map query (partial data)

---
+++ queries/overview/user_map.sql
@@ -0,0 +1,104 @@
+select 32.064378 as latitude, 34.794804 as longitude, 'דן גולן' as "שם"
+union
+select 32.064378 as latitude, 34.794804 as longitude, 'דן גולן' as "שם"
+union
+select 32.164711 as latitude, 34.833655 as longitude, 'אלון גולן' as "שם"
+union
+select 31.255988 as latitude, 34.796877 as longitude, 'אלון ספקטור' as "שם"
+union
+select 32.92403 as latitude, 35.375378 as longitude, 'אמיר' as "שם"
+union
+select 31.791712 as latitude, 34.705456 as longitude, 'אריאל גזיאל' as "שם"
+union
+select 32.118958 as latitude, 34.841752 as longitude, 'אסיף גצל' as "שם"
+union
+select 32.773567 as latitude, 35.034203 as longitude, 'אבי קוריאט' as "שם"
+union
+select 32.055854 as latitude, 34.768981 as longitude, 'אביעד כרמלי' as "שם"
+union
+select 32.604544 as latitude, 35.20249 as longitude, 'ברק גדליה' as "שם"
+union
+select 32.604544 as latitude, 35.20249 as longitude, 'ברק גדליה' as "שם"
+union
+select 32.057572 as latitude, 34.763248 as longitude, 'בן צינקין' as "שם"
+union
+select 32.066734 as latitude, 34.821752 as longitude, 'בן ברק' as "שם"
+union
+select 32.062278 as latitude, 34.795719 as longitude, 'כרמית ללוש' as "שם"
+union
+select 31.900954 as latitude, 34.806064 as longitude, 'דליה בן עטר' as "שם"
+union
+select 32.057357 as latitude, 34.771912 as longitude, 'דן גולן' as "שם"
+union
+select 31.903415 as latitude, 35.015214 as longitude, 'דני שטרק' as "שם"
+union
+select 32.082075 as latitude, 34.809824 as longitude, 'דוד גלמן (מריה)' as "שם"
+union
+select 32.091815 as latitude, 34.821314 as longitude, 'דב רשף' as "שם"
+union
+select 32.095026 as latitude, 34.780837 as longitude, 'דודו יוסף' as "שם"
+union
+select 32.055833 as latitude, 34.767881 as longitude, 'אלי דביר' as "שם"
+union
+select 31.775341 as latitude, 35.220414 as longitude, 'אלקנה אפרתי' as "שם"
+union
+select 32.088951 as latitude, 34.809702 as longitude, 'ארז קריאל' as "שם"
+union
+select 31.751707 as latitude, 35.216101 as longitude, 'בן צינקין' as "שם"
+union
+select 32.085327 as latitude, 34.975648 as longitude, 'גיל טנקוס' as "שם"
+union
+select 32.428073 as latitude, 34.885058 as longitude, 'גלעד עוזיאל' as "שם"
+union
+select 32.084745 as latitude, 34.779492 as longitude, 'אור לויעד' as "שם"
+union
+select 32.072653 as latitude, 34.774509 as longitude, 'איתי חזה מנהל של המעוז' as "שם"
+union
+select 32.072653 as latitude, 34.774509 as longitude, 'איתי חזה' as "שם"
+union
+select 32.07084 as latitude, 34.790278 as longitude, 'עידן כהן' as "שם"
+union
+select 32.065503 as latitude, 34.767901 as longitude, 'אילאיל ישראלי' as "שם"
+union
+select 32.057205 as latitude, 34.773445 as longitude, 'דן גולן' as "שם"
+union
+select 32.077585 as latitude, 34.920095 as longitude, 'ישי אטיאס' as "שם"
+union
+select 31.903784 as latitude, 35.01479 as longitude, 'ליאן עמדי' as "שם"
+union
+select 32.063097 as latitude, 34.772116 as longitude, 'מיטל נר' as "שם"
+union
+select 32.69021 as latitude, 35.048343 as longitude, 'מיראס נאטור' as "שם"
+union
+select 31.782395 as latitude, 35.214533 as longitude, 'משה אלעד' as "שם"
+union
+select 32.061944 as latitude, 34.768975 as longitude, 'נמרוד אלי' as "שם"
+union
+select 31.287517 as latitude, 34.798763 as longitude, 'נעם בריקמן' as "שם"
+union
+select 32.104432 as latitude, 34.794788 as longitude, 'עומר שייח' as "שם"
+union
+select 31.247667 as latitude, 34.799106 as longitude, 'עומרי תבורי' as "שם"
+union
+select 32.055713 as latitude, 34.770931 as longitude, 'אור שאקד' as "שם"
+union
+select 32.063966 as latitude, 34.795739 as longitude, 'אורן ׳האפס׳ קוריאט' as "שם"
+union
+select 31.25421 as latitude, 34.79524 as longitude, 'רז שטרק' as "שם"
+union
+select 31.951521 as latitude, 34.803947 as longitude, 'רינה גולן' as "שם"
+union
+select 32.077594 as latitude, 34.769182 as longitude, 'רון יעיש' as "שם"
+union
+select 31.254066 as latitude, 34.792898 as longitude, 'שקד רגב' as "שם"
+union
+select 31.99469 as latitude, 34.945888 as longitude, 'שרון פייס' as "שם"
+union
+select 31.781071 as latitude, 35.217025 as longitude, 'שי קפון' as "שם"
+union
+select 32.092398 as latitude, 34.778149 as longitude, 'שי שוורץ' as "שם"
+union
+select 31.83592 as latitude, 34.720839 as longitude, 'רועי סיון' as "שם"
+union
+select 32.021881 as latitude, 34.777734 as longitude, 'סטפן שפין' as "שם"
+;
