a1db27e7041f9056bb28b051619e1147571e6de6
Oren Koriat <oren@cynerio.co>
2020-09-27 17:55:53 +0300

    update rush hour and time of day functions

--- functions/get_time_of_day.sql
+++ functions/get_time_of_day.sql
@@ -1,18 +1,21 @@
-create or replace function get_time_of_day(in time_of_test bigint)
-returns text as $$
-declare time_of_day text;
+create or replace function rush_hour(in stamp bigint)
+returns bool as $$
+declare
+is_rush_hour bool;
 begin
-    select concat(
-            to_char(to_israel_dst_aware(time_of_test), 'Day'),
-            ' ',
-            case when extract(hour from to_israel_dst_aware(time_of_test)) between 0 and 5  then 'night'
-              when extract(hour from to_israel_dst_aware(time_of_test)) between 6 and 11  then 'morning'
-              when extract(hour from to_israel_dst_aware(time_of_test)) between 12 and 17  then 'noon'
-              when extract(hour from to_israel_dst_aware(time_of_test)) between 18 and 23  then 'evening'
-            else 'ERROR' end
-        )
-    into time_of_day;
-    return time_of_day;
+    select get_time_of_day(stamp)
+               in (
+                    'Saturday  Evening',
+                    'Thursday  Evening',
+                    'Friday    Noon',
+                    'Tuesday   Evening',
+                    'Wednesday Evening',
+                    'Sunday    Evening',
+                    'Saturday  Noon',
+                    'Monday    Evening',
+                    'Friday    Evening'
+                )
+    into is_rush_hour;
+    return is_rush_hour;
 end;
 $$ language PLpgSQL;
-
--- functions/rush_hour.sql
+++ functions/rush_hour.sql
@@ -5,14 +5,17 @@ is_rush_hour bool;
 begin
     select get_time_of_day(stamp)
                in (
-                    'saturday  evening',
-                    'tuesday   evening',
-                    'monday    evening',
-                    'thursday  evening',
-                    'sunday    evening'
+                    'Saturday  Evening',
+                    'Thursday  Evening',
+                    'Friday    Noon',
+                    'Tuesday   Evening',
+                    'Wednesday Evening',
+                    'Sunday    Evening',
+                    'Saturday  Noon',
+                    'Monday    Evening',
+                    'Friday    Evening'
                 )
     into is_rush_hour;
     return is_rush_hour;
 end;
 $$ language PLpgSQL;
-
