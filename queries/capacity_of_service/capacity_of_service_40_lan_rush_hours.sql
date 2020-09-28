9e864fdd415731a01ed4de6c20500e8bd964ba01
Oren Koriat <oren@cynerio.co>
2020-09-28 16:12:35 +0300

    update to 100+

--- queries/capacity_of_service/capacity_of_service_40_lan_rush_hours.sql
+++ queries/capacity_of_service/capacity_of_service_40_lan_rush_hours.sql
@@ -1,10 +1,9 @@
-select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
+select regexp_replace(capacity_squash_100_plus(ground_truth_rate, speed), '\d+:', '') "Capacity of Service",
        count(*)
 from valid_tests
 where speed = 40
 and connection = 'LAN'
 and rush_hour(timestamp)
-group by capacity(ground_truth_rate, speed)
-order by capacity(ground_truth_rate, speed)
+group by capacity_squash_100_plus(ground_truth_rate, speed)
+order by capacity_squash_100_plus(ground_truth_rate, speed)
 ;
-
