bb040e4e367a18b572b6d25c0e76bb2275cc3ef7
Oren Koriat <oren@cynerio.co>
2020-11-03 20:19:27 +0200

    valid tests create table for replica

---
+++ replica/create_valid_tests_table_cmd.sql
@@ -0,0 +1,14 @@
+CREATE TABLE valid_tests (
+   ground_truth_rate FLOAT,
+   speed_test_rate FLOAT,
+   user_name VARCHAR,
+   connection VARCHAR,
+   speed INT,
+   website VARCHAR,
+   file_name VARCHAR,
+   is_classic_test BOOLEAN,
+   infrastructure VARCHAR,
+   isp VARCHAR,
+   timestamp BIGINT
+);
+
