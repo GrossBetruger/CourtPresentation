4877d4c76d1d6a54a6d6cef78a90c68e6e5c162e
Oren Koriat <oren@cynerio.co>
2020-11-18 16:03:33 +0200

    add website histograms

---
+++ queries/speed_test_website_comparison/speed_test_website_comparison_histogram_google.sql
@@ -0,0 +1,20 @@
+with website_ratios as (
+    select ground_truth_rate / speed_test_rate as ratio
+    from valid_tests
+    where true_or_null(is_classic_test)
+    and speed_test_rate != 0 and ground_truth_rate != 0
+    and website  = 'google'
+    and (ground_truth_rate / speed_test_rate) between 0.01 and 100
+),
+binned_ratios as (
+    select
+      ratio_20_bins(ratio)
+       as binned_ratio
+
+    from website_ratios
+)
+select regexp_replace(binned_ratio, '\d+:', '') error_rate, count(binned_ratio) number_of_tests
+from binned_ratios
+group by binned_ratio
+order by substring(binned_ratio, '(\d+):')::int;
+
