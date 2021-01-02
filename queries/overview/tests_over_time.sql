e91bada58af259b144cde1c6e2e0b5a7f8ee726c
Oren Koriat <oren@cynerio.co>
2021-01-02 21:21:19 +0200

    update tests over time query and visulization

--- queries/overview/tests_over_time.sql
+++ queries/overview/tests_over_time.sql
@@ -4,12 +4,16 @@ with times as (
                    '/',
                    date_part('year', to_israel_dst_aware(timestamp))
                ) as timeoftest,
-           user_name
+           user_name,
+           date_part('year', to_israel_dst_aware(timestamp)) raw_year,
+           date_part('month', to_israel_dst_aware(timestamp)) raw_month
     from valid_tests
     order by timestamp
 )
-select timeoftest as "זמן דגימה", count(*) "מספר דגימות", count(distinct user_name) as "מספר משתמשים"
+select timeoftest as "זמן דגימה", count(*) "מספר דגימות", count(distinct user_name) as "מספר משתמשים",
+       raw_year, raw_month
 from times
-group by timeoftest
+group by timeoftest, raw_year, raw_month
+order by raw_year, raw_month
 ;
 
