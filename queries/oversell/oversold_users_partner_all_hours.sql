8ebf73cb8e268e5d071db6f9706284b39e2f25c7
Oren Koriat <oren@cynerio.co>
2021-01-23 23:50:51 +0200

    partner oversold users all

---
+++ queries/oversell/oversold_users_partner_all_hours.sql
@@ -0,0 +1,27 @@
+with oversell as (select 0.5),
+
+user_averages as (
+    select avg(ground_truth_rate), user_name, speed
+    from valid_tests
+    where connection = 'LAN'
+    and user_name in (select * from partner_users)
+    group by user_name, speed
+),
+
+user_oversell as (
+    select user_name,
+           case
+               when (avg / speed < (select * from oversell)) then 'משתמשים במכירת יתר'
+               when (avg / speed >= (   select * from oversell)) then 'משתמשים שאינם במכירת יתר'
+               else 'error'
+           end as oversell
+    from user_averages
+)
+
+select count(distinct user_name) number_of_users,
+       round((count(oversell) / (select count(*) from partner_users)::float)::numeric, 2) * 100 percent,
+       oversell
+from user_oversell
+group by oversell
+;
+
