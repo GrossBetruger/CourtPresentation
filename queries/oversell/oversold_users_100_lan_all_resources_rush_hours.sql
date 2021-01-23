694e0d728d4f84f97e8906279c27384d5c233d26
Oren Koriat <oren@cynerio.co>
2021-01-23 12:31:35 +0200

    update oversell query rush hours

--- queries/oversell/oversold_users_100_lan_all_resources_rush_hours.sql
+++ queries/oversell/oversold_users_100_lan_all_resources_rush_hours.sql
@@ -1,9 +1,18 @@
-with user_averages as (
+with nums as (
+    select user_name, count(*) num_tests
+    from valid_tests
+    where speed = 100
+      and connection = 'LAN'
+    group by user_name
+),
+
+user_averages as (
     select avg(ground_truth_rate), user_name, speed, count(*) number_of_tests
     from valid_tests
     where connection = 'LAN'
     and speed = 100
     and rush_hour(timestamp)
+    and user_name in (select user_name from nums where num_tests > 100)
     group by user_name, speed
 ),
 
@@ -22,6 +31,7 @@ user_oversell as (
            end as oversell
     from user_averages
 )
+
 select count(distinct user_name) number_of_users,
        round((count(oversell) / (select * from total_users)::float)::numeric, 2) * 100 percent,
        oversell
