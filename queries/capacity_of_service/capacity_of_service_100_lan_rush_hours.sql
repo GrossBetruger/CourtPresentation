bec6b3c6ca9a88c7c20078ec6cfc6448b69d942e
Oren Koriat <oren@cynerio.co>
2021-01-28 22:33:31 +0200

    squash100+ cap rush

--- queries/capacity_of_service/capacity_of_service_100_lan_rush_hours.sql
+++ queries/capacity_of_service/capacity_of_service_100_lan_rush_hours.sql
@@ -1,9 +1,10 @@
-select regexp_replace(capacity(ground_truth_rate, speed), '\d+:', ''),
+select regexp_replace(capacity_squash_100_plus(ground_truth_rate, speed), '\d+:', ''),
        count(*)
 from valid_tests
 where speed = 100
 and connection = 'LAN'
 and rush_hour(timestamp)
-group by capacity(ground_truth_rate, speed)
-order by capacity(ground_truth_rate, speed)
+group by capacity_squash_100_plus(ground_truth_rate, speed)
+order by capacity_squash_100_plus(ground_truth_rate, speed)
 ;
+
