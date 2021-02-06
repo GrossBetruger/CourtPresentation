7975a1f7829f12790cf36fccceeca8e6297953bc
Oren Koriat <oren@cynerio.co>
2021-02-06 19:44:10 +0200

    use low test view for bezeq users view

--- views/bezeq_users.sql
+++ views/bezeq_users.sql
@@ -4,17 +4,11 @@ create or replace view bezeq_users as
         select distinct user_name
         from valid_tests
         where (isp ~* 'bezeq' or infrastructure ~* 'bezeq')
-    ),
-
-    test_count as (
-     select user_name, connection, count(*) number_of_tests
-     from valid_tests
-     group by user_name, connection
     )
 
     select user_name
     from users
-    where user_name not in (select user_name from test_count where number_of_tests < 500)
+    where user_name not in (select user_name from low_test_users)
 )
 ;
 
