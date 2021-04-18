12d98b55dc74a79035be71d09d4ce472a23fcbf8
Oren Koriat <oren@cynerio.co>
2021-04-18 14:44:52 +0300

    export postgres port, remove unused cmd

--- docker_build_cmd.sh
+++ docker_build_cmd.sh
@@ -2,5 +2,5 @@ docker stop local_servile
 docker rm local_servile
 docker build -t local_servile_image . --network host
 docker volume create servile_db 
-docker run --name local_servile -v servile_db:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password local_servile_image  
+docker run --name local_servile -p 5432:5432 -v servile_db:/var/lib/postgresql/data -e POSTGRES_PASSWORD=password local_servile_image  
 
--- docker_db_init.sh
+++
@@ -1,2 +0,0 @@
-
-docker exec -it local_servile psql -U postgres -c "\copy testers from '/var/lib/postgresql/data/testers.csv'" 
