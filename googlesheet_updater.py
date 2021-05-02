f83aff6b2b92810d0d2e484fa496a566649112fb
Oren Koriat <oren@cynerio.co>
2021-05-02 21:23:47 +0300

    googlesheets

---
+++ googlesheet_updater.py
@@ -0,0 +1,18 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.expanduser('~/.servile_google_sheets_creds.json'),
+                                                         scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("test sheet").sheet1
+
+# Extract and print all of the values
+list_of_hashes = sheet.get_all_records()
+print(list_of_hashes)
