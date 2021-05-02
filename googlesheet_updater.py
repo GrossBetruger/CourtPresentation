f655d1ea9673a01dd1440f5663f77d6c3bdc5fb4
Oren Koriat <oren@cynerio.co>
2021-05-02 23:23:53 +0300

    update sheets with csv

--- googlesheet_updater.py
+++ googlesheet_updater.py
@@ -2,17 +2,22 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive.file',
+         'https://www.googleapis.com/auth/drive']
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.expanduser('~/.servile_google_sheets_creds.json'),
                                                          scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("test sheet").sheet1
 
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+def get_sheet_id_by_title(gspread_client: gspread.client.Client, title: str):
+    for sheet in gspread_client.openall():
+        if sheet.title == title:
+            return sheet.id
+    raise Exception(f"sheet not found: {title}")
+
+
+def upload_csv(spreadsheet_title: str, csv_raw: str):
+    global client
+    client.import_csv(get_sheet_id_by_title(client, spreadsheet_title), csv_raw)
