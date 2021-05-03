d3234d43d5bea18ed4c32ffa9cc82b55f6177de9
Oren Koriat <oren@cynerio.co>
2021-05-03 16:47:03 +0300

    add evening sample

--- random_samples.py
+++ random_samples.py
@@ -11,6 +11,7 @@ def create_user_stats_view():
         select user_name,
                min(to_israel_dst_aware(timestamp)) first_test,
                min(to_israel_dst_aware(timestamp)) + interval '30' day first_test_plus_30_days,
+               min(to_israel_dst_aware(timestamp)) + interval '60' day first_test_plus_60_days,
                count(*) num_test
         from valid_tests
         where connection = 'LAN'
@@ -19,6 +20,7 @@ def create_user_stats_view():
         );
         """
     )
+    engine.commit()
 
 
 def create_random_sample_table():
@@ -39,6 +41,23 @@ def create_random_sample_table():
     engine.commit()
 
 
+def create_random_sample_evening_table():
+    engine.cursor().execute("""
+    create table if not exists test_random_sample_evening(
+            id serial primary key,
+            user_name text,
+            result float,
+            speed integer,
+            isp text,
+            infra text,
+            connection text,
+            file_name text,
+            timestamp bigint)
+        ;
+    """)
+    engine.commit()
+
+
 def create_random_sample():
     global engine
     engine.cursor().execute("""
@@ -46,7 +65,7 @@ def create_random_sample():
         declare uname text;
         begin
             perform (select setseed(0.314159265359));
-            for uname in (select distinct user_name from valid_tests)
+            for uname in (select distinct user_name from valid_tests order by user_name)
             loop
            insert into test_random_sample(user_name, result, speed, isp, infra, connection, file_name, timestamp)
            select valid_tests.user_name, ground_truth_rate, speed, isp, infrastructure, connection, file_name, timestamp
@@ -64,18 +83,59 @@ def create_random_sample():
     engine.commit()
 
 
+def create_random_sample_evening():
+    global engine
+    engine.cursor().execute("""
+        do $$
+        declare uname text;
+        begin
+            perform (select setseed(0.314159265359));
+            for uname in (select distinct user_name from valid_tests order by user_name)
+            loop
+        insert into test_random_sample_evening(user_name, result, speed, isp, infra, connection, file_name, timestamp)
+           select valid_tests.user_name, ground_truth_rate, speed, isp, infrastructure, connection, file_name, timestamp
+           from   valid_tests
+           join user_stats on valid_tests.user_name = user_stats.user_name
+           where valid_tests.user_name = uname
+            and to_israel_dst_aware(timestamp) between first_test and first_test_plus_60_days
+            and connection = 'LAN'
+            and num_test >= 700
+            and is_evening(timestamp)
+           order by random() limit 300;
+        end loop;
+            end;
+        $$;
+    """)
+    engine.commit()
+
+
 def drop_random_sample():
     global engine
     engine.cursor().execute("drop table if exists test_random_sample;")
     engine.commit()
 
 
+def drop_random_sample_evening():
+    global engine
+    engine.cursor().execute("drop table if exists test_random_sample_evening")
+    engine.commit()
+
+
 if __name__ == "__main__":
     create_user_stats_view()
     drop_random_sample()
+    drop_random_sample_evening()
     create_random_sample_table()
+    create_random_sample_evening_table()
+    create_random_sample_evening()
     create_random_sample()
     cur = engine.cursor()
     cur.execute("select * from test_random_sample")
     for _ in cur.fetchall():
         print(_)
+
+    cur.execute("select * from test_random_sample_evening")
+    for _ in cur.fetchall():
+        print(_)
+
+    print("ALL DONE")
